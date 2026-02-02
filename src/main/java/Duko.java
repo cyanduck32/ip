1cb06b5d8ef6c3b238a076393dac08b11aed2128
cyanduck32 <msproton858@gmail.com>
2026-02-02 22:50:55 +0800

    add store text; display list

--- src/main/java/Duko.java
+++ src/main/java/Duko.java
@@ -4,6 +4,9 @@ public class Duko {
     static String horizontalLine = "____________________________________________________________";
     public static String greeting = "Hello! I'm Duko \nWhat can I do for you?";
     public static String closingGreeting = "Bye. Hope to see you again soon!";
+    public static String[] list = new String[100];
+    public static int listSize = 0;
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -25,11 +28,20 @@ public class Duko {
                 System.out.println (closingGreeting);
                 System.out.println(horizontalLine);
                 break;
-            }
+            } else if (input.equals("list")) {
+                System.out.println(horizontalLine);
+                for (int i = 0; i < listSize; i++) {
+                    System.out.println((i + 1) + ". " + list[i]);
+                }
+                System.out.println(horizontalLine);
+            } else {
+                list[listSize] = input;
+                listSize++;
 
-            System.out.println(horizontalLine);
-            System.out.println(input);
-            System.out.println(horizontalLine);
+                System.out.println(horizontalLine);
+                System.out.println("added: " + input);
+                System.out.println(horizontalLine);
+            }
         }
         scanner.close();
     }
