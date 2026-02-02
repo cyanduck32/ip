b653a00a7f2fa798339f10e2f9a959ee3231a1bc
cyanduck32 <msproton858@gmail.com>
2026-02-02 23:40:38 +0800

    add Task class;add mark; add unmark

--- src/main/java/Duko.java
+++ src/main/java/Duko.java
@@ -4,8 +4,8 @@ public class Duko {
     static String horizontalLine = "____________________________________________________________";
     public static String greeting = "Hello! I'm Duko \nWhat can I do for you?";
     public static String closingGreeting = "Bye. Hope to see you again soon!";
-    public static String[] list = new String[100];
-    public static int listSize = 0;
+    public static Task[] tasks = new Task[100];
+    public static int taskCount = 0;
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -29,15 +29,23 @@ public class Duko {
                 System.out.println(horizontalLine);
                 break;
             } else if (input.equals("list")) {
-                System.out.println(horizontalLine);
-                for (int i = 0; i < listSize; i++) {
-                    System.out.println((i + 1) + ". " + list[i]);
+                System.out.println(horizontalLine + "\n Here are the tasks in your list:");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
                 }
                 System.out.println(horizontalLine);
+            } else if (input.startsWith("mark")){
+                int index = Integer.parseInt(input.split(" ")[1]) - 1;
+                tasks[index].markAsDone();
+                System.out.println(horizontalLine + "\n Nice! I've marked this task as done:\n " + tasks[index] + "\n" + horizontalLine);
+            } else if (input.startsWith("unmark")) {
+                int index = Integer.parseInt(input.split(" ")[1]) - 1;
+                tasks[index].unmark();
+                System.out.println(horizontalLine + "\n OK, I've marked this task as not done yet:\n" + tasks[index] + "\n" + horizontalLine);
             } else {
-                list[listSize] = input;
-                listSize++;
-
+                tasks[taskCount] = new Task(input);
+                tasks[taskCount] = new Task(input);
+                taskCount++;
                 System.out.println(horizontalLine);
                 System.out.println("added: " + input);
                 System.out.println(horizontalLine);
