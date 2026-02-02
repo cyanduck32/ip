f4c76933655e233a2251abc839330663c98ee5ed
cyanduck32 <msproton858@gmail.com>
2026-02-02 21:58:16 +0800

    add echo

--- src/main/java/Duko.java
+++ src/main/java/Duko.java
@@ -1,4 +1,7 @@
+import java.util.Scanner;
+
 public class Duko {
+    static String horizontalLine = "____________________________________________________________";
     public static String greeting = "Hello! I'm Duko \nWhat can I do for you?";
     public static String closingGreeting = "Bye. Hope to see you again soon!";
     public static void main(String[] args) {
@@ -8,7 +11,26 @@ public class Duko {
                 + "| |_| | |_| |   < | | |\n"
                 + "|____/ \\__,_|_|\\_\\___/\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println(horizontalLine);
         System.out.println(greeting);
-        System.out.println(closingGreeting);
+        System.out.println(horizontalLine);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")){
+                System.out.println(horizontalLine);
+                System.out.println (closingGreeting);
+                System.out.println(horizontalLine);
+                break;
+            }
+
+            System.out.println(horizontalLine);
+            System.out.println(input);
+            System.out.println(horizontalLine);
+        }
+        scanner.close();
     }
 }
